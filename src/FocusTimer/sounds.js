1cba92f128e7f0f0fe36f64403ceed8413dfd50a
Alexsandro Oliveira <ale.osantos@hotmail.com>
2023-08-03 18:42:41 -0300

    Project FocusTimer

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/button-press.mp3')
+
+export const kitchenTimer = new Audio('./assets/kitchen-timer2.m4a')
+
+export const bgAudio = new Audio('./assets/Chuva.wav')
+
+bgAudio.loop = true
